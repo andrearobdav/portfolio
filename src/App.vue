e63c55c19f41c53ffe7470da845ff2d28cd5a753
Andrea Robles <cardavila13@gmail.com>
2022-06-13 00:35:22 +0200

    Improving structure and adding styles

--- src/App.vue
+++ src/App.vue
@@ -8,81 +8,90 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About me</RouterLink>
-        <RouterLink to="/portfolio">Portfolio</RouterLink>
-        <RouterLink to="/contact">Contact me</RouterLink>
+        <RouterLink to="/"> <img src="" alt="" class="nav-icon home">
+          Home</RouterLink>
+        <RouterLink to="/about"><img src="" alt="" class="nav-icon about">About me</RouterLink>
+        <RouterLink to="/portfolio"><img src="" alt="" class="nav-icon portfolio">Portfolio</RouterLink>
+        <RouterLink to="/contact"><img src="" alt="" class="nav-icon contact">Contact me</RouterLink>
       </nav>
     </div>
   </header>
 
-
-  <RouterView />
-
+  <main>
+    <RouterView />
+  </main>
 
 </template>
 
 <style lang="scss">
 @import '@/assets/base.scss';
 
-#app {
+body {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-  font-family: 'Exo 2', sans-serif;
-  background-color: var(--bg-color);
-  background-image: var(--bg-gradient);
   width: 100vw;
   height: 100vh;
+  font-family: 'Exo 2', sans-serif;
+  position: absolute;
 
-
-  header {
-    line-height: 1.5;
-    // max-height: 100vh;
-    display: flex;
-    flex-direction: column;
-
-    nav {
-      width: 100%;
-      font-size: 1rem;
-      text-align: center;
-      margin-top: 2rem;
-
-    }
-
-    nav a.router-link-exact-active {
-      color: var(--third-color);
-    }
-
-    nav a.router-link-exact-active:hover {
-      background-color: var(--third-color);
-    }
-
-
-    nav a {
-      // display: inline-block;
-      padding: 0 1rem;
-      border-left: 1px solid var(--color-border);
-      color: var(--secondary-color);
-      text-decoration: none;
-    }
-
-    nav a:first-of-type {
-      border: 0;
-    }
-  }
-
-  body {
-    display: flex;
-    flex-direction: column;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  #app {
     background-color: var(--bg-color);
     background-image: var(--bg-gradient);
     width: 100vw;
     height: 100vh;
 
+    header {
+      line-height: 1.5;
+      // max-height: 100vh;
+      display: flex;
+      flex-direction: column;
+
+      .wrapper {
+
+        nav {
+          width: 100%;
+          font-size: 1rem;
+          text-align: center;
+          margin-top: 2rem;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+        }
+
+        nav a.router-link-exact-active {
+          color: var(--third-color);
+        }
+
+        // nav a.router-link-exact-active:hover {
+        //   background-color: var(--third-color);
+        // }
+
+
+        nav a {
+          // display: inline-block;
+          // padding: 0 1rem;
+          // border-left: 1px solid var(--color-border);
+          color: var(--secondary-color);
+          text-decoration: none;
+        }
+
+        // nav a:first-of-type {
+        //   border: 0;
+        // }
+      }
+    }
+
+    main {
+      display: flex;
+      flex-direction: column;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+
+
+    }
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="about">
-    <h1 class="hello">This is a home page</h1>
+  <div class="container">
+    <h1 class="hello">Hola, soy Andrea Robles :)</h1>
+    <h2 class="hello">Front End Web Developer</h2>
   </div>
 </template>
 
 <style lang="scss" scoped>
+main {
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 80vh;
+
+
+    h1 {
+      color: var(--secondary-color);
+      font-weight: 700;
+      font-size: 2.5rem;
+      text-align: center;
+
+    }
+
+    h2 {
+      color: var(--secondary-color);
+      font-weight: 600;
+      border: solid 3px #ffffff;
+      padding: 0.5rem 1.5rem;
+      border-radius: 20px;
+    }
+  }
+}
 </style>
