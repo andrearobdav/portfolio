94d3577ee93caa4782894964dde55f886b159154
Andrea Robles <cardavila13@gmail.com>
2022-06-12 21:31:12 +0200

    Adding scss to the project, yas

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
 
@@ -22,7 +21,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style lang="scss">
-@import '@/assets/base.css';
+@import '@/assets/base.scss';
 
 #app {
   max-width: 1280px;
